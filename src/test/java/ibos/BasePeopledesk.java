700ba9750595e95b82576f28f87356ee11233db9
mehediabir1 <mehedi.abir2@gmail.com>
2022-03-01 17:11:35 +0600

    update --edgedriver

---
+++ src/test/java/ibos/BasePeopledesk.java
@@ -0,0 +1,62 @@
+package ibos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BasePeopledesk {
+    public static WebDriver driver;
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void ChromeOpen(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    public static void EdgeOpen(){
+        System.setProperty("webdriver.edge.driver","src/main/resources/msedgedriver.exe");
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        }
+
+    public static void ChromeClose(){
+        driver.quit();
+    }
+
+    public static void EdgeClose(){
+        driver.quit();
+    }
+
+    public static void GetUrl(String URL){
+        driver.get(URL);
+    }
+
+    public static WebElement findById(String Locator){
+        return driver.findElement(By.id(Locator));
+    }
+
+    public static WebElement findByClass(String Locator){
+        return driver.findElement(By.className(Locator));
+    }
+
+    public static WebElement findByCSS(String Locator){
+        return driver.findElement(By.cssSelector(Locator));
+    }
+
+    public static WebElement findByXpath(String Locator){
+        return driver.findElement(By.xpath(Locator));
+    }
+
+    public static WebElement findByName(String Locator){
+        return driver.findElement(By.name(Locator));
+    }
+
+
+}
+
---
+++ src/test/java/ibos/EmployeeManagement.java
@@ -0,0 +1,47 @@
+package ibos;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class EmployeeManagement extends BasePeopledesk{
+
+    public static void main(String[] args) {
+        ChromeOpen();
+        GetUrl("https://ibos.peopledesk.io/");
+        login_001();
+        employee_mng();
+    }
+
+    public static WebDriver driver;
+
+    public static void login_001() {
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[1]/div/input")).sendKeys("abir@ibos.io");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[2]/div/input")).sendKeys("@demo123");
+        WebElement login = driver.findElement(By.className("btn-basic"));
+        login.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    public static void employee_mng(){
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div[2]/div/div[2]/div/div")).click();
+        String searchName = "Abir";
+        System.out.println("Module: Employee Management");
+        WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[1]/ul/li[1]/div/div/input"));
+        searchBar.sendKeys(searchName);
+        searchBar.sendKeys(Keys.RETURN);
+
+        try {
+            boolean elemFound = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]/div")).isDisplayed();
+            System.out.println("Search Result Came");
+        }
+        catch (Exception e){
+            System.out.println("No Search Result");
+        }
+    }
+
+}
