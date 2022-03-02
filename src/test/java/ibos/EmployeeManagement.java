0c80c26f19c3616afdeb1023ec046b1f5954ef34
mehediabir1 <mehedi.abir2@gmail.com>
2022-03-02 18:24:07 +0600

    update

--- src/test/java/ibos/EmployeeManagement.java
+++ src/test/java/ibos/EmployeeManagement.java
@@ -13,21 +13,39 @@ public class EmployeeManagement extends BasePeopledesk{
     public static void main(String[] args) {
         ChromeOpen();
         GetUrl("https://ibos.peopledesk.io/");
-        login_001();
-        employee_mng();
+        login();
+        employee_mng_001();
+        employee_mng_002();
     }
-
     public static WebDriver driver;
 
-    public static void login_001() {
+    public static void login() {
+        driver.navigate().refresh();
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[1]/div/input")).sendKeys("abir@ibos.io");
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[2]/div/input")).sendKeys("@demo123");
         WebElement login = driver.findElement(By.className("btn-basic"));
         login.click();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
+//Search for Invalid Name
+    public static void employee_mng_001(){
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div[2]/div/div[2]/div/div")).click();
+        String searchName = "Zee";
+        System.out.println("Module: Employee Management");
+        WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[1]/ul/li[1]/div/div/input"));
+        searchBar.sendKeys(searchName);
+        searchBar.sendKeys(Keys.RETURN);
 
-    public static void employee_mng(){
+        try {
+            boolean elemFound = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]/div")).isDisplayed();
+            System.out.println("Search Result Came");
+        }
+        catch (Exception e){
+            System.out.println("No Search Result");
+        }
+    }
+    //Search for Valid Name
+    public static void employee_mng_002(){
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div[2]/div/div[2]/div/div")).click();
         String searchName = "Abir";
         System.out.println("Module: Employee Management");
@@ -43,5 +61,4 @@ public class EmployeeManagement extends BasePeopledesk{
             System.out.println("No Search Result");
         }
     }
-
 }
