6067bd7d37e9d83cc86092d81ef3666036e5f6fd
mehediabir1 <mehedi.abir2@gmail.com>
2022-03-09 16:57:38 +0600

    update

--- src/test/java/ibos/login.java
+++ src/test/java/ibos/login.java
@@ -15,7 +15,8 @@ public class login {
     public static void main(String[] args) {
         ChromeOpen();
         login_001();
-        login_002();
+        Logout();
+        //login_002();
         //employee_mng();
     }
 
@@ -34,33 +35,57 @@ public class login {
         login.click();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
-        driver.findElement(By.className("profile-menu-img")).click();
-        driver.findElement(By.className("single-profile-popup-list-txt")).click();
-        System.out.println("Logged Out");
+       // driver.findElement(By.className("profile-menu-img")).click();
+       // driver.findElement(By.xpath("//*[@id=\"simple-popover\"]/div[3]/div/ul/li[2]")).click();
+        System.out.println("Test Passed - Login_001");
+    }
+
+    public static void Logout(){
+        if (driver.findElement(By.className("profile-menu-img"))!= null){
+            System.out.println("logout present");
+            driver.findElement(By.className("profile-menu-img")).click();
+            driver.findElement(By.xpath("//*[@id=\"simple-popover\"]/div[3]/div/ul/li[2]")).click();
+        }
+        else
+        {
+            System.out.println("logout NOT Found");
+        }
+        //driver.findElement(By.className("profile-menu-img")).click();
+        //driver.findElement(By.xpath("//*[@id=\"simple-popover\"]/div[3]/div/ul/li[2]")).click();
     }
 
     //-- LOGIN WITH IN-ACTIVE USER ACCOUNT CREDENTIAL --//
     public static void login_002(){
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[1]/div/input")).sendKeys("abir@ibos.io");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[1]/div/input")).sendKeys("demo@ibos.io");
         driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[2]/div/input")).sendKeys("@demo123");
         WebElement login = driver.findElement(By.className("btn-basic"));
         login.click();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        System.out.println("Test Passed - Login_002");
     }
-    public static void employee_mng(){
-        driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div[2]/div/div[2]/div/div")).click();
-        String searchName = "Abir";
-        System.out.println("Module: Employee Management");
-        WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[1]/ul/li[1]/div/div/input"));
-        searchBar.sendKeys(searchName);
-        searchBar.sendKeys(Keys.RETURN);
-
-        try {
-            boolean elemFound = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]/div")).isDisplayed();
-            System.out.println("Search Result Came");
-        }
-        catch (Exception e){
-            System.out.println("No Search Result");
-        }
+//    public static void employee_mng(){
+//        driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div[2]/div/div[2]/div/div")).click();
+//        String searchName = "Abir";
+//        System.out.println("Module: Employee Management");
+//        WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[1]/ul/li[1]/div/div/input"));
+//        searchBar.sendKeys(searchName);
+//        searchBar.sendKeys(Keys.RETURN);
+//
+//        try {
+//            boolean elemFound = driver.findElement(By.xpath("//*[@id=\"main\"]/div[5]/div/form/div[2]/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]/div")).isDisplayed();
+//            System.out.println("Search Result Came");
+//        }
+//        catch (Exception e){
+//            System.out.println("No Search Result");
+//        }
+//    }
+    //-- LOGIN WITH WRONG USER ACCOUNT CREDENTIAL --//
+    public static void login_003(){
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[1]/div/input")).sendKeys("demo541@ibos.io");
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div/form/div[1]/div/div/div[2]/div/div[2]/div/input")).sendKeys("@demo123");
+        WebElement login = driver.findElement(By.className("btn-basic"));
+        login.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        System.out.println("Test Passed - Login_002");
     }
 }
